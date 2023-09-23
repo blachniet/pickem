333b055d0e0e75ae42811622e86f5ed3339adbf3
Brian Lachniet <blachniet@gmail.com>
2023-09-23 09:47:01 -0400

    Add script to create week

---
+++ new-weekly.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+default_season="$(date +%Y)"
+echo -n "Season [${default_season}]: "
+read season
+if [[ "${season}" = '' ]]; then
+  season="${default_season}"
+fi
+
+echo -n "Week (e.g. 1, 2, 3): "
+read week
+
+bundle="weeks/${season}w$(printf '%02i' "${week}")"
+
+hugo new "${bundle}"
+open "https://www.espn.com/nfl/schedule/_/week/${week}/year/${season}/"
+nvim "content/${bundle}/data.csv"
